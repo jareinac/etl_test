0199f330086300f3cc25c13216de3d4ee579c3bc
jareinac <76272414+jareinac@users.noreply.github.com>
2025-09-08 08:06:28 -0500

    Update redshift-ddl.sql

--- sql/redshift-ddl.sql
+++ sql/redshift-ddl.sql
@@ -1,4 +1,7 @@
--- DDL compatible with Redshift / DuckDB
+--la tabla fact_order sigue el enfoque de un esquema estrella, donde fact_order almacena métricas transaccionales (importe, cantidad, fecha) y se relaciona con dimensiones externas (usuarios).
+--Llave primaria: order_id asegura unicidad a nivel de transacción.
+--El volumen esperado y la naturaleza de las consultas aún no requiere definir claves de distribución/ordenamiento hasta conocer los patrones de uso.
+--La estrategia sería usar un SORTKEY(created_date) para acelerar filtros por fecha
 CREATE TABLE dim_user (
   user_id VARCHAR(64) PRIMARY KEY,
   email VARCHAR(255),
